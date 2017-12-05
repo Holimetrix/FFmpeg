23b5d4a70f7f16649d40ae448cfb484a33071aba
Alexandre Quivy <alexandre.quivy@epitech.eu>
2017-12-05 10:57:39 +0100

    strftime_ms support milliseconds with %L

---
+++ libavutil/strftime_ms.h
@@ -0,0 +1,12 @@
+#ifndef STRFTIME_MS_H_
+# define STRFTIME_MS_H_
+
+# include <time.h>
+# include <sys/time.h>
+
+size_t	strftime_ms(char* ptr,
+		    size_t maxsize,
+		    const char* format,
+		    const struct timeval* tv);
+
+#endif /* !STRFTIME_MS_H_ */
